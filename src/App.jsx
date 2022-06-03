b18399059257c6f1d143411b06cf043f9c462aa3
Poe <pwas123@gmail.com>
2022-06-03 17:00:12 -0400

    Added buttons and have to dynamically change square size, need to figure out dom manipulation in react

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,42 @@ import Board from './components/Board'
 function App() {
 
 
-  const handleClick = () => {
+  function handleClick() {
     console.log('Clicked')
   }
+  function changeColor(e){
+    e.target.style.background = 'orange';
+  }
+  function resetSquares(){
+    document.getElementsByClassName('square').style.background='black'
+  }
+  function changeSquares(e){
+    console.log(e.target)
+    if(e.target.id === 'sixteen'){
+      //document.getElementsByClassName('squares').style.background
+    }
+    else if(e.target.id === 'sixfour'){
 
+    }
+    else if(e.target.id === 'onetwoeight'){
+
+    }
+  }
 
   return (
-    <div className='full'>
-      <Board onClick={()=>handleClick} />
+    <div>
+      <div className='container'>
+        <div>
+          <div className='button' id='reset' onClick={resetSquares}>Reset</div>
+          <div className='button' id='sixteen' onClick={(e)=>{changeSquares(e)}}>16x16</div>
+          <div className='button' id='sixfour' onClick={(e)=>{changeSquares(e)}}>64x64</div>
+          <div className='button' id='onetwoeight' onClick={(e)=>{changeSquares(e)}}>128x128</div>
+        </div>
+        <Board onClick={()=>handleClick} onHover={changeColor} />
+      </div>
+      <div>
+
+      </div>
     </div>
   );
 }
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,22 +1,20 @@
 const Board = (props) => {
 
-    function changeColor(e){
-        e.target.style.background = 'red';
-    }
 
 
     console.log(props)
     let board = [];
-    for(let i = 0; i<64; i++){
+    for(let i = 0; i<16; i++){
         let boardRow = [];
-        for(let j = 0; j<64; j++){
-            boardRow.push(<div className="square" onMouseEnter={changeColor} onClick={props.onClick()} key={i.toString()+j.toString()}></div>)
+        for(let j = 0; j<16; j++){
+            boardRow.push(<div className="square" onMouseEnter={(e)=>{props.onHover(e)}} onClick={props.onClick()} key={i.toString()+j.toString()}></div>)
         }
-        board.push(<div key={i}>{boardRow}</div>)
+        board.push(<div className="board-row" key={i}>{boardRow}</div>)
     }
-    
+ 
+   
     return(
-        <div className="board">
+        <div className="flex-grid">
             {board}
         </div>
     );
