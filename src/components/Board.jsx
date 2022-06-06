c8a709f84239a5e57099fcfbbb45cba701b81555
Poe <pwas123@gmail.com>
2022-06-06 19:00:46 -0400

    Added the click and mouse to colour in the sketch instead of hover

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,14 +1,11 @@
-import React, { startTransition, useRef, useState } from 'react';
-import createBoard from '../helper/createBoard';
+import React, { useRef, useState } from 'react';
 
 const Board = (props) => {
-    const [board, setBoard] = useState([createBoard(props)]);
-    const [bgColor, setBgColor] = useState('');
+    const [board, setBoard] = useState([createBoard()]);
+    const isDrawing = useRef(false);
     const flexGridRef = useRef(null);
     let amountOfSquares = 16;
     let boxLength = 0;
-    let height = '0px';
-    let width = '0px'
 
     function resetSquares(){
         let squares = document.getElementsByClassName('square');
@@ -17,23 +14,43 @@ const Board = (props) => {
         }
     }
 
-    //let board = [];
+    
     function changeSquares(e){
-
         boxLength = flexGridRef.current.clientHeight/parseInt(e.target.id);
         amountOfSquares = parseInt(e.target.id);
         let newBoard = [];
         for(let i = 0; i<amountOfSquares; i++){
             let boardRow = [];
             for(let j = 0; j<amountOfSquares; j++){
-                boardRow.push(<div className="square" style={{height:boxLength, width:boxLength}} onMouseDown={(e)=>{props.changeColor(e)}} key={i.toString()+j.toString()}></div>)
+                boardRow.push(<div className="square" style={{height:boxLength, width:boxLength}}  onClick={(e)=>{props.changeColor(e)}} onMouseDown={()=>{isDrawing.current=true}} onMouseUp={()=>{isDrawing.current=false}} onMouseOver={(e)=>{
+                    console.log(isDrawing.current);
+                    if(isDrawing.current){
+                        props.changeColor(e)
+                    }
+                }} key={i.toString()+j.toString()}></div>)
             }
             newBoard.push(<div className="board-row" key={i}>{boardRow}</div>)
 
         }
         setBoard(newBoard);
     }
-
+    
+    function createBoard(){
+        let starterBoard = [];
+        for(let i = 0; i<16; i++){
+            let boardRow = [];
+            for(let j = 0; j<16; j++){
+                boardRow.push(<div className="square" style={{height:'50px', width:'50px'}}  onClick={(e)=>{props.changeColor(e)}} onMouseDown={()=>{isDrawing.current=true}} onMouseUp={()=>{isDrawing.current=false}} onMouseEnter={(e)=>{
+                    console.log(isDrawing.current);
+                    if(isDrawing.current){
+                        props.changeColor(e)
+                    }
+                }} key={i.toString()+j.toString()}></div>)
+            }
+            starterBoard.push(<div className="board-row" key={i}>{boardRow}</div>)
+        }
+        return starterBoard;
+    }
 
     return(
         <div>
@@ -41,7 +58,7 @@ const Board = (props) => {
                 <div className='button' id='reset' onClick={()=>{resetSquares()}}>Reset</div>
                 <div className='button' id='16' onClick={(e)=>{changeSquares(e);}}>16x16</div>
                 <div className='button' id='64' onClick={(e)=>{changeSquares(e);}}>64x64</div>
-                <div className='button' id='128' onClick={(e)=>{changeSquares(e); }}>128x128</div>
+                <div className='button' id='100' onClick={(e)=>{changeSquares(e); }}>100x100</div>
             </div>
             <div ref={flexGridRef} className="flex-grid">
                 {board}
